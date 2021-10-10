0058ae7e9400f2c56f581410e56689147f8c4cf2
mitali2608 <mitalibawankule21@gmail.com>
2021-10-11 00:24:19 +0530

    MoodAnalyser Class Added

---
+++ MoodAnalyserClass.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day20_MoodAnalyser
+{
+    public class MoodAnalyserClass
+    {
+        public string message;
+        public MoodAnalyserClass()
+        {
+
+        }
+        public MoodAnalyserClass(string msg)
+        {
+            this.message = msg;
+        }
+
+        public string AnalyseMood()
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new CustomException(CustomException.ExceptionType.Entered_Empty_String, "String is Empty");
+                }
+
+                if (this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                throw new CustomException(CustomException.ExceptionType.Entered_null, "Nothing is entered");
+            }
+        }
+
+    }
+}
