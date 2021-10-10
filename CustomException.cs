efc795648c59644ffd1a33e89d74931fbcb8752c
mitali2608 <mitalibawankule21@gmail.com>
2021-10-11 00:20:12 +0530

    Custom Class added

---
+++ CustomException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day20_MoodAnalyser
+{
+    public class CustomException:Exception
+    {
+        public enum ExceptionType
+        {
+            Entered_null,
+            Entered_Empty_String,
+            No_Such_Class,
+            No_Such_Method,
+            No_Such_Field
+        }
+
+        ExceptionType enumtype;
+
+        public CustomException(ExceptionType type, string msg) : base(msg)
+        {
+            this.enumtype = type;
+        }
+    }
+}
