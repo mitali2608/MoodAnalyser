0f686e224db91fe6548cb9f13064ae77a6dd3509
mitali2608 <mitalibawankule21@gmail.com>
2021-10-11 00:20:51 +0530

    Main class Added

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day20_MoodAnalyser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
